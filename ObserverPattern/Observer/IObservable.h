7a469216792aa5f2daf44d8192a0beb7a6589a8b
Coderast <Coderast@example.com>
2018-07-19 04:21:29 +0300

    Interfaces completed

--- ObserverPattern/Observer/IObservable.h
+++ ObserverPattern/Observer/IObservable.h
@@ -1,7 +1,11 @@
 #pragma once
 
+#include "IObserver.h"
+
 class IObservable
 {
 public:
 	virtual void notifyObservers() = 0;
+	virtual void addObserver(IObserver *) = 0;
+	virtual void removeObserver(IObserver *) = 0;
 };
